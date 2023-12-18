43ec75444ddc51b1329708c2cd1a49ed0daf1f46
Abdullah <afzalabdullah067@gmail.com>
2023-12-18 14:11:58 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+app.use(express.json());
+
+// Replace <username>, <password>, and <dbname> with your MongoDB Atlas username, password, and database name
+const mongoUri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+ if (err) throw err;
+
+ const collection = client.db("<dbname>").collection("devices");
+
+ app.post('/data', (req, res) => {
+  console.log(req.body);
+
+  collection.insertOne(req.body, function(err, result) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    res.sendStatus(200);
+  });
+ });
+
+ app.listen(8080, () => console.log('Server listening on port 8080'));
+});
