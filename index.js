77a4e2a5dbb7d38f7ad77676d6b5a68245f7bb97
Abdullah-TPL <153810267+Abdullah-TPL@users.noreply.github.com>
2023-12-26 13:15:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -75,34 +75,36 @@ const MongoClient = require('mongodb').MongoClient;
 app.use(express.json());
 
 // Replace <username>, <password>, and <dbname> with your MongoDB Atlas username, password, and database name
-const uri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 async function connectToMongoDB() {
- try {
-   const client = new MongoClient(uri);
-   await client.connect();
-   const collection = client.db("<dbname>").collection("devices");
+  try {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+    const collection = client.db("<dbname>").collection("devices");
 
-   app.post('/data', (req, res) => {
-     console.log(req.body);
+    async function handlePostRequest(req, res) {
+      try {
+        console.log(req.body);
 
-     collection.insertOne(req.body, function (err, result) {
-       if (err) {
-         console.error("Error inserting document:", err);
-         client.close();
-         res.status(500).send("Internal Server Error");
-       } else {
-         console.log("1 document inserted");
-         client.close();
-         res.sendStatus(200);
-       }
-     });
-   });
+        const result = await collection.insertOne(req.body);
+        console.log("1 document inserted");
+        res.sendStatus(200);
+      } catch (err) {
+        console.error("Error inserting document:", err);
+        res.status(500).send("Internal Server Error: " + err.message);
+      } finally {
+        client.close();
+      }
+    }
 
-   app.listen(8080, () => console.log('Server listening on port 8080'));
- } catch (err) {
-   console.error('Failed to connect to MongoDB:', err);
- }
+    app.post('/data', handlePostRequest);
+
+    app.listen(8080, () => console.log('Server listening on port 8080'));
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err);
+  }
 }
 
 connectToMongoDB();
+
