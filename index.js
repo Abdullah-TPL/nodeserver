3936766f51e4bbe13af68691256246b8351a562a
Abdullah-TPL <153810267+Abdullah-TPL@users.noreply.github.com>
2023-12-27 18:06:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 //     await client.connect();
 //     isConnected = true;
 
-//     const collection = client.db("BLE_Gateway").collection("devices");
+//     const collection = client.db("<dbname>").collection("devices");
 
 //     async function handlePostRequest(req, res) {
 //       try {
@@ -26,7 +26,13 @@
 //           throw new Error("MongoDB client is not connected");
 //         }
 
-//         const result = await collection.insertOne(req.body);
+//         // Add a timestamp to the data before inserting it
+//         const dataWithTimestamp = {
+//           ...req.body,
+//           timestamp: new Date()
+//         };
+
+//         const result = await collection.insertOne(dataWithTimestamp);
 //         console.log("1 document inserted");
 //         res.sendStatus(200);
 //       } catch (err) {
@@ -44,7 +50,6 @@
 // }
 
 // connectToMongoDB();
-
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
@@ -87,7 +92,22 @@ async function connectToMongoDB() {
       }
     }
 
+    async function handleGetRequest(req, res) {
+      try {
+        if (!isConnected) {
+          throw new Error("MongoDB client is not connected");
+        }
+
+        const data = await collection.find({}).toArray();
+        res.json(data);
+      } catch (err) {
+        console.error("Error retrieving data:", err);
+        res.status(500).send("Internal Server Error: " + err.message);
+      }
+    }
+
     app.post('/data', handlePostRequest);
+    app.get('/data', handleGetRequest); // New endpoint for retrieving data
 
     app.listen(8080, () => console.log('Server listening on port 8080'));
   } catch (err) {
