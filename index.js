2b1aa4c583b24c5587b09c49c57662d63ed26927
Abdullah-TPL <153810267+Abdullah-TPL@users.noreply.github.com>
2023-12-27 16:52:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,49 @@
 
-
 // const express = require('express');
 // const app = express();
-// const MongoClient = require('mongodb').MongoClient;
+// const { MongoClient } = require('mongodb');
 
 // app.use(express.json());
 
 // // Replace <username>, <password>, and <dbname> with your MongoDB Atlas username, password, and database name
-// const uri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
-// async function connectToMongoDB() {
-//  try {
-//    const client = new MongoClient(uri);
-//    await client.connect();
-//    const collection = client.db("<dbname>").collection("devices");
-
-//    app.post('/data', (req, res) => {
-//      console.log(req.body);
-
-//      collection.insertOne(req.body, function(err, result) {
-//        if (err) throw err;
-//        console.log("1 document inserted");
-//        res.sendStatus(200);
-//      });
-//    });
-
-//    app.listen(8080, () => console.log('Server listening on port 8080'));
-//  } catch (err) {
-//    console.error('Failed to connect to MongoDB:', err);
-//  }
-// }
-
-// connectToMongoDB();
-
-// const express = require('express');
-// const app = express();
-// const MongoClient = require('mongodb').MongoClient;
-
-// app.use(express.json());
-
-// // Replace <username>, <password>, and <dbname> with your MongoDB Atlas username, password, and database name
-// const uri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://MERN:mernstack@cluster0.36bh8a0.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 // async function connectToMongoDB() {
 //   try {
-//     const client = new MongoClient(uri);
+//     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     let isConnected = false;
+
 //     await client.connect();
+//     isConnected = true;
+
 //     const collection = client.db("<dbname>").collection("devices");
 
-//     app.post('/data', (req, res) => {
-//       console.log(req.body);
+//     async function handlePostRequest(req, res) {
+//       try {
+//         console.log(req.body);
 
-//       collection.insertOne(req.body, function (err, result) {
-//         if (err) {
-//           console.error("Error inserting document:", err);
-//           res.status(500).send("Internal Server Error");
-//         } else {
-//           console.log("1 document inserted");
-//           res.sendStatus(200);
+//         if (!isConnected) {
+//           throw new Error("MongoDB client is not connected");
 //         }
-//       });
-//     });
+
+//         const result = await collection.insertOne(req.body);
+//         console.log("1 document inserted");
+//         res.sendStatus(200);
+//       } catch (err) {
+//         console.error("Error inserting document:", err);
+//         res.status(500).send("Internal Server Error: " + err.message);
+//       }
+//     }
+
+//     app.post('/data', handlePostRequest);
 
 //     app.listen(8080, () => console.log('Server listening on port 8080'));
 //   } catch (err) {
-//     console.error('Failed to connect to MongoDB:', err);
+//     console.error('Failed to initialize MongoDB connection:', err);
 //   }
 // }
+
+// connectToMongoDB();
 
 const express = require('express');
 const app = express();
@@ -94,7 +72,13 @@ async function connectToMongoDB() {
           throw new Error("MongoDB client is not connected");
         }
 
-        const result = await collection.insertOne(req.body);
+        // Add a timestamp to the data before inserting it
+        const dataWithTimestamp = {
+          ...req.body,
+          timestamp: new Date()
+        };
+
+        const result = await collection.insertOne(dataWithTimestamp);
         console.log("1 document inserted");
         res.sendStatus(200);
       } catch (err) {
@@ -112,3 +96,4 @@ async function connectToMongoDB() {
 }
 
 connectToMongoDB();
+
