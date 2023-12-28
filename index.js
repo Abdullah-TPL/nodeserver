fbac22de323e95ef7d868bb44be81629538a80d0
Abdullah-TPL <153810267+Abdullah-TPL@users.noreply.github.com>
2023-12-28 13:17:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -67,30 +67,64 @@ async function connectToMongoDB() {
     await client.connect();
     isConnected = true;
 
-    const collection = client.db("<dbname>").collection("devices");
+    const collection = client.db("BLE_Gateway").collection("devices");
 
+    // async function handlePostRequest(req, res) {
+    //   try {
+    //     console.log(req.body);
+
+    //     if (!isConnected) {
+    //       throw new Error("MongoDB client is not connected");
+    //     }
+
+    //     // Add a timestamp to the data before inserting it
+    //     const dataWithTimestamp = {
+    //       ...req.body,
+    //       timestamp: new Date()
+    //     };
+
+    //     const result = await collection.insertOne(dataWithTimestamp);
+    //     console.log("1 document inserted");
+    //     res.sendStatus(200);
+    //   } catch (err) {
+    //     console.error("Error inserting document:", err);
+    //     res.status(500).send("Internal Server Error: " + err.message);
+    //   }
+    // }
     async function handlePostRequest(req, res) {
-      try {
-        console.log(req.body);
+  try {
+    console.log(req.body);
 
-        if (!isConnected) {
-          throw new Error("MongoDB client is not connected");
-        }
-
-        // Add a timestamp to the data before inserting it
-        const dataWithTimestamp = {
-          ...req.body,
-          timestamp: new Date()
-        };
-
-        const result = await collection.insertOne(dataWithTimestamp);
-        console.log("1 document inserted");
-        res.sendStatus(200);
-      } catch (err) {
-        console.error("Error inserting document:", err);
-        res.status(500).send("Internal Server Error: " + err.message);
-      }
+    if (!isConnected) {
+      throw new Error("MongoDB client is not connected");
     }
+
+    const { data } = req.body;
+
+    // Extract the last part after splitting with '09'
+    const lastPart = data.split('09').pop();
+
+    // Parse the hexadecimal data to UTF-8
+    const parsedData = Buffer.from(lastPart, 'hex').toString('utf-8');
+    
+    console.log("Parsed Data:", parsedData);
+
+    // Add a timestamp to the data before inserting it
+    const dataWithTimestamp = {
+      ...req.body,
+      parsedData,
+      timestamp: new Date()
+    };
+
+    const result = await collection.insertOne(dataWithTimestamp);
+    console.log("1 document inserted");
+    res.sendStatus(200);
+  } catch (err) {
+    console.error("Error inserting document:", err);
+    res.status(500).send("Internal Server Error: " + err.message);
+  }
+}
+
 
     async function handleGetRequest(req, res) {
       try {
